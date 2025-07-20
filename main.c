77feb1f40f71f4753a0fe5a02fc0efd1c82bb86c
Martijn Terpstra <bigmartijn@gmail.com>
2025-07-20 14:30:17 +0200

    Change behaviour to a simple sierpinsky triangle

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include "FreeRTOS.h"
+#include "semphr.h"
 #include "task.h"
 #include <stdint.h>
 
@@ -11,6 +12,12 @@
 #define UART0_BASE 0x4000C000
 #define UART_DR    (*(volatile uint32_t*)(UART0_BASE + 0x000))
 #define UART_FR    (*(volatile uint32_t*)(UART0_BASE + 0x018))
+
+
+
+SemaphoreHandle_t global_mtx; // Mutex handle
+#define data_size 80
+static char data[data_size+3];
 
 // System initialization
 void SystemInit(void) {
@@ -32,42 +39,43 @@ void uart_puts(const char* str) {
     }
 }
 
-// Hello World task
-void hello_task(void *pvParameters) {
-    int counter = 0;
-
-    uart_puts("FreeRTOS Hello World started!\r\n");
-
+// printing task
+void print_task(void *pvParameters) {
     while (1) {
-        uart_puts("Hello World from FreeRTOS! Counter: ");
-
-        // Simple integer to string conversion
-        char num_str[16];
-        int temp = counter;
-        int i = 0;
-
-        if (temp == 0) {
-            num_str[i++] = '0';
-        } else {
-            char temp_str[16];
-            int j = 0;
-            while (temp > 0) {
-                temp_str[j++] = '0' + (temp % 10);
-                temp /= 10;
-            }
-            // Reverse the string
-            for (int k = j - 1; k >= 0; k--) {
-                num_str[i++] = temp_str[k];
-            }
-        }
-        num_str[i] = '\0';
-
-        uart_puts(num_str);
-        uart_puts("\r\n");
-
-        counter++;
+      if (xSemaphoreTake(global_mtx, portMAX_DELAY) == pdTRUE) {
+        uart_puts(data);
+        xSemaphoreGive(global_mtx);
         vTaskDelay(pdMS_TO_TICKS(1000)); // Delay 1 second
+      }
     }
+}
+
+void update_task(void *pvParameters) {
+  /* spooky non-mutex data preparation. */
+  for(int i=0;i<data_size;i++)
+    data[i]=' ';
+  data[0]='#';                /* set one byte to one for interesting behaviour */
+
+  data[data_size  ]='\r'; // set last bytes to allow printing
+  data[data_size+1]='\n';
+  data[data_size+2]='\0';
+
+  char newdata[data_size];
+  while (1) {
+    if (xSemaphoreTake(global_mtx, portMAX_DELAY) == pdTRUE) {
+      for(int i=0;i<data_size;i++) {
+          newdata[i]=0;
+          if(data[(data_size+i+1)%data_size]==' ') newdata[i]^=1;
+          if(data[(data_size+i-1)%data_size]==' ') newdata[i]^=1;
+        }
+        for(int i=0;i<data_size;i++)
+          data[i]=(newdata[i]==1)?'#':' ';
+
+      xSemaphoreGive(global_mtx);
+      vTaskDelay(pdMS_TO_TICKS(1000)); // Delay 1 second
+    }
+
+  }
 }
 
 // FreeRTOS hook functions
@@ -97,13 +105,28 @@ int main(void) {
 
     uart_puts("System starting...\r\n");
 
-    // Create the hello world task
+    global_mtx = xSemaphoreCreateMutex(); // Create the mutex
+    if (global_mtx == NULL) {
+          uart_puts("Could not initialize mutex\r\n");
+    }
+
+    // Create the printing task
     xTaskCreate(
-        hello_task,           // Task function
-        "HelloTask",          // Task name
+        print_task,           // Task function
+        "PrintToUART",          // Task name
         configMINIMAL_STACK_SIZE * 2,  // Stack size
         NULL,                 // Parameters
         tskIDLE_PRIORITY + 1, // Priority
+        NULL                  // Task handle
+    );
+
+    // Create the update task
+    xTaskCreate(
+        update_task,           // Task function
+        "UpdateData",          // Task name
+        configMINIMAL_STACK_SIZE * 2,  // Stack size
+        NULL,                 // Parameters
+        tskIDLE_PRIORITY + 2, // Priority
         NULL                  // Task handle
     );
 
